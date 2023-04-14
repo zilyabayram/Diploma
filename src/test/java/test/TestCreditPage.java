55b78c7e0dcae4e7f9ce9798c1e0b79526b7b0d2
Zilia Bairamova <ziliabairamova@gmail.com>
2023-04-14 14:39:33 +0300

    tests added

--- src/test/java/test/TestCreditPage.java
+++ src/test/java/test/TestCreditPage.java
@@ -1,6 +1,5 @@
 package test;
 
-
 import com.codeborne.selenide.logevents.SelenideLogger;
 import data.DataHelper;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -10,18 +9,15 @@ import page.MainPage;
 import static com.codeborne.selenide.Selenide.open;
 import static data.DBHelper.cleanDataBase;
 
-public class TestPayPage {
-
+public class TestCreditPage {
     @BeforeEach
     public void openPage() {
         open("http://localhost:8080/");
     }
-
     @BeforeAll
     static void setUpAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
-
     @AfterAll
     static void tearDownAll() {
         SelenideLogger.removeListener("allure");
@@ -31,25 +27,26 @@ public class TestPayPage {
     public void tearDown() {
         cleanDataBase();
     }
+
     @Test
-    void testBuyUsingApprovedCardWithValidInformationAndReturnSuccessNotification() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithValidInformationAndReturnSuccessNotification() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithValidInformation();
         payPage.initializeCard(cardInfo);
         payPage.notificationSuccess();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithInvalidMonth() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithInvalidMonth() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithInvalidMonth();
         payPage.initializeCard(cardInfo);
         payPage.notificationInvalidExpirationDate();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithEmptyMonth() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithEmptyMonth() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithEmptyMonth();
         payPage.initializeCard(cardInfo);
         payPage.notificationInvalidString();
@@ -57,116 +54,81 @@ public class TestPayPage {
 
     @Test
     void testBuyUsingApprovedCardWithMonth00() {
-        var payPage = new MainPage().buyDebit();
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithMonth00();
         payPage.initializeCard(cardInfo);
         payPage.notificationInvalidString();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithInvalidYear() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithInvalidYear() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithInvalidYear();
         payPage.initializeCard(cardInfo);
         payPage.notificationInvalidExpirationDate();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithPastYear() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithPastYear() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithPastYear();
         payPage.initializeCard(cardInfo);
         payPage.notificationPastYear();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithEmptyYear() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithEmptyYear() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithEmptyYear();
         payPage.initializeCard(cardInfo);
         payPage.notificationInvalidString();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithEmptyCardHolder() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithEmptyCardHolder() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithEmptyCardHolder();
         payPage.initializeCard(cardInfo);
         payPage.notificationEmptyCardHolder();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithCardHolderNameOnly() {
-        var payPage = new MainPage().buyDebit();
-        var cardInfo = DataHelper.generateApprovedCardWithCardHolderNameOnly();
-        payPage.initializeCard(cardInfo);
-        payPage.notificationInvalidString();
-    }
-
-  @Test
-    void testBuyUsingApprovedCardWithRusCardHolder() {
-        var payPage = new MainPage().buyDebit();
-        var cardInfo = DataHelper.generateApprovedCardWithRusCardHolder();
-        payPage.initializeCard(cardInfo);
-        payPage.notificationInvalidString();
-    }
-
-    @Test
-    void testBuyUsingApprovedCardWithCardHolderWithNumbers() {
-        var payPage = new MainPage().buyDebit();
-        var cardInfo = DataHelper.generateApprovedCardWithCardHolderWithNumbers();
-        payPage.initializeCard(cardInfo);
-        payPage.notificationInvalidString();
-    }
-
-    @Test
     void testBuyUsingApprovedCardWithLongCardHolderName() {
-        var payPage = new MainPage().buyDebit();
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithLongCardHolderName();
         payPage.initializeCard(cardInfo);
         payPage.notificationInvalidString();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithInvalidCVV() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithInvalidCVV() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithInvalidCVV();
         payPage.initializeCard(cardInfo);
         payPage.notificationInvalidString();
     }
 
     @Test
-    void testBuyUsingApprovedCardWithEmptyCVV() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingApprovedCardWithEmptyCVV() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateApprovedCardWithEmptyCVV();
         payPage.initializeCard(cardInfo);
         payPage.notificationInvalidString();
     }
 
     @Test
-    void testBuyUsingCardWithIncompleteNumber() {
-        var payPage = new MainPage().buyDebit();
-        var cardInfo = DataHelper.generateCardWithIncompleteCardNumber();
-        payPage.initializeCard(cardInfo);
-        payPage.notificationInvalidString();
-    }
-
-    @Test
-    void testBuyUsingDeclinedCardWithValidInformationAndReturnErrorNotification() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingDeclinedCardWithValidInformationAndReturnErrorNotification() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateDeclinedCardWithValidInformation();
         payPage.initializeCard(cardInfo);
         payPage.notificationError();
-
     }
 
     @Test
-    void testBuyUsingCardWithRandomNumber() {
-        var payPage = new MainPage().buyDebit();
+    void testBuyCreditUsingCardWithRandomNumberAndReturnErrorNotification() {
+        var payPage = new MainPage().buyCredit();
         var cardInfo = DataHelper.generateCardWithRandomNumber();
         payPage.initializeCard(cardInfo);
         payPage.notificationError();
     }
-
-
 }
